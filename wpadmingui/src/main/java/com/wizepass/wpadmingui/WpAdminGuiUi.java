92bc7421467429d86800f3748e64267f0f1a7289
janjira <janjira.eriksson@gmail.com>
2017-02-27 21:22:34 +0100

    Added getDistinguishedName method for search userdata

--- wpadmingui/src/main/java/com/wizepass/wpadmingui/WpAdminGuiUi.java
+++ wpadmingui/src/main/java/com/wizepass/wpadmingui/WpAdminGuiUi.java
@@ -1,6 +1,8 @@
 package com.wizepass.wpadmingui;
 
 import com.vaadin.annotations.Theme;
+import com.vaadin.data.Property;
+import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
@@ -8,6 +10,8 @@ import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.TreeTable;
 import com.vaadin.ui.UI;
@@ -23,6 +27,10 @@ import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.annotation.WebServlet;
 
@@ -30,55 +38,86 @@ import javax.servlet.annotation.WebServlet;
 public class WpAdminGuiUi extends UI {
     final VerticalLayout layout = new VerticalLayout();
     final Button buttonSearch = new Button();
-   final Button buttonCreateRegToken = new Button("Create Registation Token");
+    final Button buttonCreateRegToken = new Button("Create Registation Token");
+    private final Logger logger = Logger.getLogger(WpAdminGuiUi.class.getName());
+    final JSONObject jsonObj = HttpClientRequest.loadLdapData();
  
     @Override
     protected void init(final VaadinRequest vaadinRequest) {
 
-        final TreeTable treeTable = createTreeTable();
-        final TextField textFieldSearch = searchView();
-        final Label showData = showDataFromSearch(textFieldSearch);
+        final TreeTable treeTable = createTreeTable(jsonObj);
+        final TextField textFieldSearch = searchView(jsonObj);
         buttonCreateRegToken.setEnabled(false);
-        layout.addComponents( treeTable, buttonCreateRegToken, textFieldSearch, showData);
+        layout.addComponents( treeTable, buttonCreateRegToken, textFieldSearch);
         layout.setMargin(true);
         layout.setSpacing(true);
         setContent(layout);
     }
     
-    private TextField searchView() {
-        final TextField textFieldSearch = new TextField("Search :");
-        textFieldSearch.setWidth("30%");
-        textFieldSearch.getValue();
-        return textFieldSearch;
+    private TextField searchView(final JSONObject jsonObj) {
+    	 final String objStr = jsonObj.toString();
+         final TextField tf = new TextField("Search :");
+         tf.setWidth("40%");
+         tf.focus();
+         tf.setInputPrompt("Write some text ");
+         tf.addValueChangeListener(new Property.ValueChangeListener() {
+             private static final long serialVersionUID = 5678485350989070188L;
+             @Override
+             public void valueChange(final ValueChangeEvent event) {
+                     String value = (String) event.getProperty().getValue();
+                     List<Object> list = (List<Object>) getDistinguishedName(jsonObj);
+                     String listStr = list.toString();
+                     if ( listStr.contains(value)) {
+                         Notification.show(value, Type.TRAY_NOTIFICATION);
+                         logger.log(Level.INFO, " Get user data from UserDbApi");
+                     } else {
+                         Notification.show("Not found ",value, Type.TRAY_NOTIFICATION);
+                     }
+             }
+         });
+         tf.setImmediate(true);
+         return tf;
+     }
+
+    private List<Object> getDistinguishedName(final JSONObject jsonObj) {
+        final List<Object> listObj = new LinkedList<Object>();
+        final JSONObject attributes = (JSONObject) jsonObj.get("attributes");
+       // final JSONArray dis = (JSONArray) attributes.get("distinguishedName");
+
+        String disStr = "";
+        if (attributes != null && attributes.get("distinguishedName") != null) {
+            //final JSONArray dis = (JSONArray) attributes.get("distinguishedName");
+            for (int i = 0; i < attributes.size(); i++) {
+                disStr += attributes.get(i);
+            }
+            logger.log(Level.INFO, "Yoooo ");
+            listObj.add(disStr);
+        } else {
+            logger.log(Level.INFO, "Not found ");
+            listObj.add(null);
+        }
+        return listObj;
     }
 
-    @SuppressWarnings("unused")
-    private Label showDataFromSearch( final TextField text) {
-        final Label label = new Label();
-        TextField textfield = searchView();
-        label.setValue(textfield.getValue());
-        return label;
-    }
 
-    private TreeTable createTreeTable() {
+    private TreeTable createTreeTable(final JSONObject jsonObj) {
     	
         final TreeTable treeTable = new TreeTable();
-
+        logger.entering(getClass().getSimpleName(), "createTreeTable");
         treeTable.addContainerProperty("LDAP Tree", CheckBox.class, "");
         treeTable.addContainerProperty("Search Base", String.class, null);
         treeTable.addContainerProperty("Given Name", String.class, null);
         treeTable.addContainerProperty("Surname", String.class, null);
         treeTable.setWidth("70%");
         treeTable.setColumnExpandRatio((Object) "LDAP Tree", 1.0f);
-
-        final RdnTypeParser parser = new RdnTypeParserRoot();
+        // start with Root first ( Root is override from RdnTypeParser ) 
+        final RdnTypeParser rdnTypeParser = new RdnTypeParserRoot();
         try {
-            final JSONObject jsonObj = HttpClientRequest.loadLdapData();
-            // try to do process Json's parent and children  by giving jsonObject from file , treetable and start row -1 
-            parser.parse(jsonObj, treeTable, -1);
+            rdnTypeParser.parse(jsonObj, treeTable, -1);
         } catch (Exception e) {
-            System.err.println("Unexpected exception: " + e);
+        	 logger.log(Level.WARNING, " Http response error ", e.getMessage());
         }
+        // create treeTable
         return treeTable;
     }
 
