b3b0768a378ce74192557a3f7620b995b98daa14
janjira <janjira.eriksson@gmail.com>
2017-05-23 15:04:07 +0200

    Changed directory of json file

--- src/main/java/com/wizepass/WpAdminGui/util/Constants.java
+++ src/main/java/com/wizepass/WpAdminGui/util/Constants.java
@@ -1,16 +1,17 @@
 package com.wizepass.WpAdminGui.util;
 
 public class Constants {
-	public static final String ALL_USERS_LABB3 = "./all_users_labb3.json";
-	public static final String TEST = "./test";
-	public static final String ALL_USERS_MGAD = "./all_user_mgad.json"; // there is no json 
-    public static final String CUSTOMERS = "./customer.json";
-    public static final String DB_ANVI = "./db_anvi.json";
-    public static final String DB_MGAB = "./db_mgab.json";
-    public static final String PROVIDER = "./provider.json";
-    public static final String CERT_PROFILE = "./cert_profile.json";
-    public static final String REG_TOKEN = "./reg_token.json";
-    public static final String USER_MG = "./users.json";
+	public static final String LOCAL_JSON = "/Users/janjiraeriksson/code/workspace/WpAdminGui/wpadmingui/wpadmingui/jsonFile/";
+	public static final String ALL_USERS_LABB3 = LOCAL_JSON + "all_users_labb3.json";
+	public static final String TEST = LOCAL_JSON + "test.json";
+	public static final String ALL_USERS_MGAD = LOCAL_JSON + "all_user_mgad.json"; // there is no json 
+    public static final String CUSTOMERS = LOCAL_JSON + "customer.json";
+    public static final String DB_ANVI = LOCAL_JSON + "db_anvi.json";
+    public static final String DB_MGAB = LOCAL_JSON + "db_mgab.json";
+    public static final String PROVIDER = LOCAL_JSON + "provider.json";
+    public static final String CERT_PROFILE = LOCAL_JSON + "cert_profile.json";
+    public static final String REG_TOKEN = LOCAL_JSON + "reg_token.json";
+    public static final String USER_MG = LOCAL_JSON + "users.json";
     
     public static final String URL_ADDRESS = "http://localhost:8081";
     public static final String WP_REST = "/WpRest/users/";
